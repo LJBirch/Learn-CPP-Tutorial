73863529c74cc78e52bc77c9b7659f3163743e4a
Lewis Birch <l.birch@lancaster.ac.uk>
2022-08-15 16:33:45 +0100

    CPP213 code for chapter 2.13 How to design your first programs.

---
+++ CPP213_Designing_Your_First_Programs.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+int main()
+{
+	// Plan out your program before you start, this will save a lot of frustration.
+
+	// Step 1: Define your goal
+	// - Should be able to state this in sentence or two.
+	// - Often useful to express this as a user-facing outcome.
+	
+	// Step 2: Define requirments
+	// - Requirements are the constraints that your solutions needs to abide (could be budget, timeline, space, memory, etc...).
+	// - Its also the capabilities the program must exhibit in order to meet the users' needs.
+	// - Should be focused on the "what", not the "how".
+
+	// Step 3: Define your tools, targets and backup plan
+	// - This could be any of the following (or even more):
+	// - Defining what target architecture and/or OS your program will run on.
+	// - Determining what set of tools you will be using.
+	// - Determining whether you will write your program alone or as part of a team.
+	// - Defining your testing / feedback / release strategy.
+	// - Determining how you will back up your code.
+
+	// Step 4: Break hard problems down into easy problems
+	// - Take complicated tasks and use the top down or bottom up method to break them down.
+	// - Break hard subtasks down even further when possible.
+	// - By continously doing this, we end up with many manageable tasks that make up the complex task.
+	// - Using the top down or bottom up method allows us to organise the tasks into hierarchies.
+
+	// Step 5: Figure out the sequence of events
+	// - Once the program has a structure, its time to determine how link all the tasks together.
+	// - You should determine the sequences of events that will be performed.
+
+	// Implementation step 1:
+	// - Outline your main function.
+	// - Comment out your function calls until you're ready to address this (or stub out your functions, so the program will compile).
+	
+	// Implementation step 2:
+	// - Define your function prototype (inputs and ouputs).
+	// - Write the function.
+	// - Test the function.
+
+	// Implementation step 3:
+	// - Test the whole program to ensure it works.
+
+	// Advice:
+	// - Keep your programs simple to start with.
+	// - Add features over time.
+	// - Focus on one area at a time.
+	// - Test each piece of code as you go.
+	// - Dont invest in perfecting early code.
+
+	return 0;
+}
