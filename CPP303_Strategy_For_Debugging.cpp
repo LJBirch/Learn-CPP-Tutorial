de7ced21817e8f25905cfce846df1bdbc66ecfd1
Lewis Birch <l.birch@lancaster.ac.uk>
2022-08-16 11:03:24 +0100

    CPP303 code for chapter 3.3 Basic debugging tactics.

---
+++ CPP303_Strategy_For_Debugging.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main()
+{
+	// Finding problems by running the program:
+	// 1) Figure out how to reproduce the problem.
+	// 2) Run the program and gather information to narrow down where the problem is.
+	// 3) Repeat the prior step until you find the problem.
+
+	return 0;
+}
