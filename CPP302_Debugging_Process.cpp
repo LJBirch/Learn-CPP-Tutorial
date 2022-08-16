df9854d74202969569bf13615e84920a2b8ba848
Lewis Birch <l.birch@lancaster.ac.uk>
2022-08-16 10:53:53 +0100

    CPP302 code for chapter 3.2 The debugging process.

---
+++ CPP302_Debugging_Process.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+int main()
+{
+	// How do we debug a program?
+
+	// For basic programs:
+
+	/*
+		Once a problem has been identified, debugging the problem generally consists of five steps:
+
+		1) Find the root cause of the problem (usually the line of code that’s not working).
+
+		2) Ensure you understand why the issue is occurring.
+
+		3) Determine how you’ll fix the issue.
+
+		4) Repair the issue causing the problem.
+
+		5) Retest to ensure the problem has been fixed and no new problems have emerged.
+	*/
+
+	return 0;
+}
