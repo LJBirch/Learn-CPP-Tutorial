fdd838fc07e70c94037ab3e013914f29ed0173d2
Lewis Birch <l.birch@lancaster.ac.uk>
2022-08-24 15:11:32 +0100

    CPP401 code for chapter 4.1 Introduction to fundamental data types.

---
+++ CPP401_Introduction_Fundamental_Data_Types.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+int main()
+{
+	// The smallest unit of memory is a binary digit (also called a bit).
+	// It holds a value of either 0 or 1.
+	// A random segment of memory would therefore look something like this: 011010100101010.
+	
+	// Memory is organized into sequential units called memory addresses.
+	// This tells us the location at which a specific contents is located in memory.
+	
+	// Each memory address holds 1 byte of data.
+	// A byte is a group of bits that are operated on as a unit.
+	// The modern standard for a byte is 8 sequential bits.
+
+	// As all data on a computer is just a sequence of bits, we use a data type to tell the compiler how to interpret the contents of memory in some meaningful way.
+	// Values in C++ get converted into a sequence of bits or retrieve sequence of bits to convert to values.
+	// The inner workings of this are carried out by the compilier and the CPU.
+
+	// C++ comes with a set of built-in support for many different data types.
+	// These are referred to as fundamental data types (often called basic types, primitive types, or built-in types).
+	
+	// See https://www.learncpp.com/cpp-tutorial/introduction-to-fundamental-data-types/ for a full list of fundamental data types.
+
+	return 0;
+}
