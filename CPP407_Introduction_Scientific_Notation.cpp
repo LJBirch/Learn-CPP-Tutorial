3bd714126949da30ab7f676a431414cc17b20737
Lewis Birch <l.birch@lancaster.ac.uk>
2022-08-25 10:48:09 +0100

    CPP407 code for chapter 4.7 Introduction to scientific notation.

---
+++ CPP407_Introduction_Scientific_Notation.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int main()
+{
+	// Scientific notation is shorthand for writing lengthy numbers in a concise manner.
+	// Numbers in scientific notation take the following form: significand x 10^exponent.
+	// Example: 1.2 x 10⁴ evaluates to 12,000.
+
+	// Numbers written in scientific notation are written one digit before the decimal point, and the rest of the digits afterwards.
+	// Example: 5973600000000000000000000 kg (mass of planet earth) in scientific notation would be 5.9736 x 10²⁴ kg.
+	// The latter is much easier to read that the former.
+
+	// In C++ to show exponents we use the letter 'e' instead.
+	// Example: 5.9736 x 10²⁴ would be 5.9736e24.
+
+	// Exponents can also be negative.
+	// Example: 5e-2 is equivalent to 5 * 10⁻², which is 5 / 10², or 0.05.
+
+	return 0;
+}
